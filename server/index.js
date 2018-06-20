a90b0cc47bf2e3eccb4d6550b60cb9b0f21fae14
Solomon Antoine <Antoinesolomon5@gmail.com>
2018-06-19 21:10:14 -0500

    First Commit from command line

---
+++ server/index.js
@@ -0,0 +1,39 @@
+//Dependencies: 
+//yarn add express cors twilio 
+
+const express = require('express'); 
+const cors = require('cors');
+const twilio = require('twilio'); 
+
+//twilio requirements -- Texting API 
+const accountSid = '___YOUR___ACCOUNT__SID';
+const authToken = '___YOUR___AUTHENTICATION__TOKEN'; 
+const client = (accountSid, authToken);
+
+const app = express(); //alias
+
+app.use(cors()); //Blocks browser from restricting any data
+
+//Welcome Page for the Server 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express Server')
+})
+
+//Twilio 
+app.get('/send-text', (req, res) => {
+    //Welcome Message
+    res.send('Hello to the Twilio Server')
+
+    //_GET Variables
+    const { recipient, textmessage } = req.query;
+
+
+    //Send Text
+    client.messages.create({
+        body: textmessage,
+        to: recipient,  // Text this number
+        from: '+15074734314' // From a valid Twilio number
+    }).then((message) => console.log(message.body));
+})
+
+app.listen(4000, () => console.log("Running on Port 4000"))
---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    text: {
+      recipient: '',
+      textmessage: ''
+    }
+  }
+
+  sendText = _ => {
+    const { text } = this.state;
+    //pass text message GET variables via query string
+    fetch(`http://127.0.0.1:4000/send-text?recipient=${text.recipient}&textmessage=${text.textmessage}`)
+    .catch(err => console.error(err))
+  }
+
+  render() {
+    const { text } = this.state;
+    const spacer = {
+      margin: 10
+    }
+    const textArea = {
+      borderRadius: 4
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div style={{ marginTop: 10 }} >
+          <h2> Send Text Message </h2>
+          <label> Your Phone Number </label>
+          <br />
+          <input value={text.recipient}
+            onChange={e => this.setState({ text: { ...text, recipient: e.target.value } })} />
+          <div style={spacer} />
+          <label> Message </label>
+          <br />
+          <textarea rows={3} value={text.textmessage} style={textArea}
+            onChange={e => this.setState({ text: { ...text, textmessage: e.target.value } })} />
+          <div style={spacer} />
+          <button onClick={this.sendText}> Send Text </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
